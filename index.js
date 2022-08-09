76faaa30644078b2d765c85d5fbdaa4509ef4d00
Shane99dt <dushen1999thilaksha@gmail.com>
2022-08-09 23:05:13 +0200

    base done with the front

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app =express()
 const port = 5000
+const studentsRoute = require('./routes/students')
+const cors = require('cors')
+const { json } = require('express')
+
+// lets the front end access my server
+app.use(cors())
+
+// lets doing requests with the database
+app.use(express.json())
+
+app.use('/students', studentsRoute)
 
 app.listen(port, (req, res) =>{
   console.log(`Server started at port ${port}`)
---
+++ routes/students.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const studentsInfo = require('../studentsInfo')
+
+
+// get
+app.get('/', (req, res) => {
+  res.json(studentsInfo)
+})
+
+app.get('/:id', (req, res) => {
+
+  const {id} = req.params
+  const requestedStudent = studentsInfo.find(student => student.id === Number(id))
+
+  try{
+    res.json(requestedStudent)
+  }catch{
+    res.send("Not Found")
+  }
+})
+
+
+
+
+
+
+// post
+app.post('/', (req, res) => {
+  const student = {
+    name: req.body.name,
+    age: req.body.age,
+    id: studentsInfo.length + 1
+  }
+
+  studentsInfo.push(student)
+  res.json(student)
+})
+
+
+
+
+
+
+module.exports = app
